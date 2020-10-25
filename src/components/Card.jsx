a4546bb971bc896713face581b328d8645abe205
theo.atlani@gmail.com <theo.atlani@gmail.com>
2020-10-25 17:57:50 +0100

    test with simple css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import styles from './Card.module.css';
+import './Card.css'
 
 const Card = (props) => {
-    
+
     return (
-        <section className={styles.card}>
-            <div className={styles.album_info}>
+        <section className="card">
+            <div className="album_info">
                 <img src={props.image} alt={props.name} />
-                <div className={styles.info}>
+                <div className="info">
                     <h1>{props.artist}</h1>
                     <div>
                         <p>{props.name}</p>
@@ -15,14 +15,14 @@ const Card = (props) => {
                     </div>
                 </div>
             </div>
-            <div className={styles.rating}>
+            <div className="rating">
                 <p>Rate this album</p>
-                <div className={styles.stars}>
-                    <span className={styles.star}>&#9734;</span>
-                    <span className={styles.star}>&#9734;</span>
-                    <span className={styles.star}>&#9734;</span>
-                    <span className={styles.star}>&#9734;</span>
-                    <span className={styles.star}>&#9734;</span>
+                <div className="stars">
+                    <span className="star">&#9734;</span>
+                    <span className="star">&#9734;</span>
+                    <span className="star">&#9734;</span>
+                    <span className="star">&#9734;</span>
+                    <span className="star">&#9734;</span>
                 </div>
             </div>
         </section>
